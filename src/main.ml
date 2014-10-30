70cc164de6d26fd450f47cb04b17b8dbd9fafbfd
Eugene Kalinin <e.v.kalinin@gmail.com>
2014-10-30 22:51:59 +0300

    moved command args parsing in 'parse_options' fn

--- src/main.ml
+++ src/main.ml
@@ -4,10 +4,9 @@
 let version = "0.1.0"
 
 (**
- *  Entry point
+ * Parses command line args and returns options
  *)
-let () =
-    (* Parse command line args *)
+let parse_options =
     let show_version = ref false in
     let work_dir = ref "." in
     let output_format = ref "%{type}/%{branch}%{dirty}%{stats}" in
@@ -24,17 +23,23 @@ let () =
                 ] in
 
     let _ = Arg.parse speclist others usage in
+        (!show_version, !work_dir, !output_format)
+
+(**
+ *  Entry point
+ *)
+let () =
+    let (show_version, dir, fmt) = parse_options in
 
     (* Show version *)
-    if !show_version
+    if show_version
     then print_endline version
     (* Show info about version control *)
     else
-        let dir = !work_dir in
         match Scm.get_type_for_dir dir with
         | None -> print_endline ""
         | Some scm ->
-            let o1 = Str.replace_first (Str.regexp "%{type}") scm !output_format
+            let o1 = Str.replace_first (Str.regexp "%{type}") scm fmt
             (*
             let branch = get_branch dir scm
             and o2 = Str.replace_first (Str.regexp "%{branch}") branch o1
